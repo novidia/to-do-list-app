2add97a87a576ee52d084e22264038039ce27ec8
Novidia <87478105+novidia@users.noreply.github.com>
2021-09-01 19:26:26 +0200

    Update Scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
+function newItem() {
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(document.createTextNode(inputValue));
 
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        let list = document.querySelector('#list');
+        $('#list').append(li);
+    }
+
+    li.on('dblclick', function crossOut() {
+        li.addClass('strike');
+    });
+
+    let crossOutButton = $('<crossOutButton> x </crossOutButton>');
+    li.append(crossOutButton);
+
+    crossOutButton.on('click', function deleteListItem() {
+        li.addClass("delete");
+    });
+
+    $('#list').sortable()
+}
